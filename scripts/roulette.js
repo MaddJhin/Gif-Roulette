6f1559677d7362a12e87afd7233b160c7e934e8a
Francisco Carrera <franciscocarrer@gmail.com>
2017-08-31 19:12:58 -0400

    Added To Do, basic variables, and basic html elements for initial testing

---
+++ scripts/roulette.js
@@ -0,0 +1,31 @@
+var key = '';
+var search = '';
+var queryURL = '';
+
+var searches =[];
+
+$(document).ready(function(){
+
+    function renderButtons(){
+
+    }
+
+    function makeButton(){
+
+    }
+
+    // TO DO: 
+    // Set a form
+        // User can either hit enter or press button
+
+    // When user enters value
+        // Take Value from form to use as search parameter and clear form
+        // Save value to button
+        // Generate Button
+
+    // When user clicks on button
+        // Request Information for Giphy
+        // Get 
+
+
+});
