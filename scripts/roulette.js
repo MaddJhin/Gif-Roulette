d5fe952153c7f4c2eba7eed40a00c1aae1cc1130
Francisco Carrera <franciscocarrer@gmail.com>
2017-09-01 01:35:34 -0400

    Buttons with data, class, text, and on click function are now generated when users submit information

--- scripts/roulette.js
+++ scripts/roulette.js
@@ -2,30 +2,47 @@ var key = '';
 var search = '';
 var queryURL = '';
 
-var searches =[];
+var searches =["cat", "dog"];
 
 $(document).ready(function(){
 
-    function renderButtons(){
+    RenderButtons();
 
+    // On Search button click
+    $('#search-add').on("click", function(){
+        // Get searched term and push to button array and clear search
+        var gifSearch = $('#search-input').val().trim();
+        $('#search-input').val("");
+        searches.push(gifSearch);
+        console.log(gifSearch);
+        RenderButtons();
+    });
+
+    // Generates buttons with desired values
+    function RenderButtons(){
+        $('#search-history').empty();
+        // For every value in the search history array
+        for (var i = 0; i < searches.length; i++)
+        {
+            // Make a button with data, text, class and on Click event to search
+            b = $('<button>');
+            b.text(searches[i])
+                .addClass("search-bttn")
+                .attr("data-name", searches[i])
+                .on("click", Search);
+
+            // Append the button to search history panel
+            $('#search-history').append(b);
+        }
     }
 
-    function makeButton(){
+    // When user clicks on button request Information for Giphy
+    function Search(){
+        var name = $(this).attr("data-name");
+        console.log("Searching", name);
 
+        // TO DO:
+        // Ajax query for name var
+        // Append gifs to main area
     }
-
-    // TO DO: 
-    // Set a form
-        // User can either hit enter or press button
-
-    // When user enters value
-        // Take Value from form to use as search parameter and clear form
-        // Save value to button
-        // Generate Button
-
-    // When user clicks on button
-        // Request Information for Giphy
-        // Get 
-
-
 });
